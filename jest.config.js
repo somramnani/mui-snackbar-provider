6842d7af378c1778625cd31b8e7e4717af30b74a
somramnani <som.ramnani@gmail.com>
2025-07-10 14:57:52 -0400

    fix(coverage): move jest config from package.json to jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,15 @@
 module.exports = {
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["./setupTest.js"],
+  collectCoverage: true,
+  coverageDirectory: "./coverage",
+  coverageReporters: ["json", "lcov", "text"],
+  coverageThreshold: {
+    global: {
+      branches: 10,
+      functions: 10,
+      lines: 10,
+      statements: 10,
+    },
+  },
 };
